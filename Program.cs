4a6e2ef3ee6c4ed6ce03b771ba8c71b7c04caf99
Ivan Machamer <ivan.machamer@students.mq.edu.au>
2025-10-27 20:59:50 +1100

    Exercise 1 finished.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Redox_Code_Test
 {
@@ -6,8 +8,36 @@ namespace Redox_Code_Test
     {
         static void Main(string[] args)
         {
-            
+            List<int> list = new List<int>();
+            for (int i = 1; i < 100; i++)
+            {
+                list.Add(i);
+            }
 
+            printEven(list);
+            printThreeOrFive(list);
+        }
+
+        static void printEven(List<int> list) 
+        {
+            IEnumerable<int> isEven = from item in list where item % 2 == 0 select item;
+            String result = "";
+            foreach (int item in isEven)
+            {
+                result+=item + ", ";
+            }
+            Console.WriteLine(result.Substring(0, result.Length - 2));
+
+        }
+
+        static void printThreeOrFive(List<int> list) 
+        {
+            String result = "";
+            foreach (int item in list) 
+            {
+                if(((item%3==0)&&(item%5!=0))||((item%3!=0)&&(item%5==0))) result+=item+", ";
+            }
+            Console.WriteLine(result.Substring(0,result.Length-2));
         }
     }
 }
